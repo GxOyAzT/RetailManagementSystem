9d63390f1ef8b29a038b8545660f27d019ffb6b5
Jakub Koza <jakub.koza13@gmail.com>
2020-10-31 00:24:51 +0100

    DeleteShopStorageFromShopModel
    Delete unnecessary pop ShopStorage from ShopModel.

--- Models/EFModels/ShopModel.cs
+++ Models/EFModels/ShopModel.cs
@@ -8,7 +8,6 @@ namespace Models
         public Guid Id { get; set; }
         public string City { get; set; }
         public string Street { get; set; }
-        public int StorageCapacity { get; set; }
 
         public virtual ICollection<EachShopModel> EachShopModels { get; set; }
         public virtual ICollection<EachShopChangesModel> EachShopChangesModels { get; set; }
