054e59062ffb99dd7c7e779de32313ac42f0d359
Jakub Koza <jakub.koza13@gmail.com>
2020-11-17 14:35:43 +0100

    UserInterfaceDesignImProvementv2
    Commit css and js files

---
+++ OrdersRegisterWeb/wwwroot/scripts/ManageOrderProductsScript.js
@@ -0,0 +1,48 @@
+﻿function IncrementOrderQuantity(iterator) {
+    var QuantityInput = document.getElementById("QuantityInput" + iterator);
+    var actualQua = parseInt(QuantityInput.value);
+    actualQua++;
+    if (actualQua > 100) {
+        return;
+    }
+    else {
+        QuantityInput.value = actualQua;
+        CalculateTotalForDistinctProduct(actualQua, iterator);
+        CalculateTotalForWholeOrder();
+    }
+}
+
+function DecrementOrderQuantity(iterator) {
+    var QuantityInput = document.getElementById("QuantityInput" + iterator);
+    var actualQua = parseInt(QuantityInput.value);
+    actualQua--;
+    console.log("XD");
+    if (actualQua < 0) {
+        return;
+    }
+    else {
+        QuantityInput.value = actualQua;
+        CalculateTotalForDistinctProduct(actualQua, iterator);
+        CalculateTotalForWholeOrder();
+    }
+}
+
+function CalculateTotalForDistinctProduct(quantity, iterator) {
+    var price = parseFloat(document.getElementById('InputBoxPrice' + iterator).value);
+    console.log(quantity + "  " + price);
+    document.getElementById("InputTotalProductPrice" + iterator).value = String((price * quantity).toFixed(2)) + " $";
+}
+
+function CalculateTotalForWholeOrder() {
+    total = 0.0;
+    for (i = 0; true; i++) {
+        if (document.getElementById('QuantityInput' + i) == null) {
+            break;
+        }
+        var quantity = parseInt(document.getElementById('QuantityInput' + i).value);
+        var price = parseFloat(document.getElementById('InputBoxPrice' + i).value);
+        total += quantity * price;
+        console.log(quantity.toString() + " " + price.toString() + " " + total.toString());
+    }
+    document.getElementById('InputOrderTotal').value = String(total.toFixed(2)) + " $";
+}
