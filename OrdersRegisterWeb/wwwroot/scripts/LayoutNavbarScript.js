b91aeabb1aca2f6769992f331048efdd6f84f0c9
Jakub Koza <jakub.koza13@gmail.com>
2020-11-21 17:56:18 +0100

    NavBarHidden
    Create function for hiding navBar on button click and vice versa.

---
+++ OrdersRegisterWeb/wwwroot/scripts/LayoutNavbarScript.js
@@ -0,0 +1,19 @@
+﻿var isHidden = false;
+
+function MoveNavigationBar() {
+    var navbar = document.getElementById('LeftNavigationBarId');
+    var leftBlank = document.getElementById('LeftBlankId');
+    var rightContent = document.getElementById('RightContentId');
+    if (isHidden == false) {
+        navbar.style.transform = "translateX(-200px)";
+        leftBlank.style.width = "50px";
+        rightContent.style.width = "calc(100% - 50px)";
+        isHidden = true;
+    }
+    else {
+        navbar.style.transform = "translateX(0px)";
+        leftBlank.style.width = "250px";
+        rightContent.style.width = "calc(100% - 250px)";
+        isHidden = false;
+    }
+}
